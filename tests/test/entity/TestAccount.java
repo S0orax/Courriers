4abc4a3fdd50f828cf8de873155b3cd69311ccea
S0orax <ttreplay@gmail.com>
2015-11-07 11:15:00 +0100

    Ajout de JUnit dans le build path

--- tests/test/entity/TestAccount.java
+++ tests/test/entity/TestAccount.java
@@ -1,5 +1,14 @@
 package test.entity;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class TestAccount {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
 
 }
